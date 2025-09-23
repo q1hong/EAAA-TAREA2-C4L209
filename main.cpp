1fac7d9dedc9f353a7691d7826adb29805a360eb
q1hong <kevinstudios9@gmail.com>
2025-09-23 15:33:57 -0600

    Definicion de clases

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(){
+
 	return 0;
 }
+// Base
+class Componente { 
+	
+};
+
+class Motor : public Componente{
+
+};
+
+class Carroceria : public Componente{
+
+};
+
+class Transmision : public Componente{
+
+};
+
+// Componentes exclusivos
+class Aleron : public Componente{
+
+};
+
+class Chasis : public Componente{
+
+};
+
+class Manillar : public Componente{
+
+};
+
+// Interfaz para competidores
+class ICompetidor{
+
+};
+
+// Base
+class Vehiculo{
+
+};
+
+class Automovil : public Vehiculo{
+
+};
+
+// Vehiculos derivados
+class Deportivo : public Automovil, public  ICompetidor{
+
+};
+
+class Sedan : public Automovil {
+
+};
+
+class Camioneta : public Vehiculo{
+
+};
+
+class Motocicleta : public Vehiculo, public ICompetidor{
+
+};
+
+
